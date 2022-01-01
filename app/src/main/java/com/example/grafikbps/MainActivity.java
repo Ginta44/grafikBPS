6c1e44207d71fe655f18c8e3ed684b5c7ac0d951
Ginta44 <tinambunanginta@gmail.com>
2022-01-01 16:10:49 +0700

    dashboard

---
+++ app/src/main/java/com/example/grafikbps/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.grafikbps;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import android.os.Bundle;
+
+import com.etebarian.meowbottomnavigation.MeowBottomNavigation;
+
+public class MainActivity extends AppCompatActivity {
+    MeowBottomNavigation bottomNavigation;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        bottomNavigation = findViewById(R.id.bottom_navigation);
+        bottomNavigation.add(new MeowBottomNavigation.Model(1,R.drawable.ic_map));
+        bottomNavigation.add(new MeowBottomNavigation.Model(2,R.drawable.ic_home));
+        bottomNavigation.add(new MeowBottomNavigation.Model(3,R.drawable.ic_info));
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.frame_layout, new HomeFragment()).commit();
+
+        bottomNavigation.setOnShowListener(new MeowBottomNavigation.ShowListener() {
+            @Override
+            public void onShowItem(MeowBottomNavigation.Model item) {
+                Fragment fragment = null;
+
+                switch (item.getId()){
+                    case 1:
+                        fragment = new MapFragment();
+                        break;
+
+                    case 2:
+                        fragment = new HomeFragment();
+                        break;
+
+                    case 3:
+                        fragment = new AboutFragment();
+                        break;
+                }
+                loadFragment(fragment);
+            }
+        });
+        bottomNavigation.setOnClickMenuListener(new MeowBottomNavigation.ClickListener() {
+            @Override
+            public void onClickItem(MeowBottomNavigation.Model item) {
+            }
+        });
+        bottomNavigation.setOnReselectListener(new MeowBottomNavigation.ReselectListener() {
+            @Override
+            public void onReselectItem(MeowBottomNavigation.Model item) {
+            }
+        });
+    }
+
+    private void loadFragment(Fragment fragment) {
+        getSupportFragmentManager()
+                .beginTransaction()
+                .replace(R.id.frame_layout,fragment)
+                .commit();
+    }
+}
